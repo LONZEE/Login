6b99c66707eff55c6d462fadfcd3b95105076f13
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-15 14:39:01 -0500

    working Tree

--- client/server/index.js
+++ client/server/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+app.use('/', require('./routes/auth'));
 
 const port = 5000;
 app.listen(port, () => {
---
+++ client/server/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express'); // Express
+
+const router = express.Router();    // Express Router
+
+const cors = require('cors');       // CORS
+const { test } = require('../controllers/authController'); // Controller
+
+//middleware
+router.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+));
+
+router.get('/', test);
+
+module.exports = router;
