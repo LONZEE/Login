52dd47c892a170ceac5775220b0d2727fd66d4e5
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-15 14:05:10 -0500

    added routes to login and register

--- client/vite-project/src/components/pages/Login.jsx
+++ client/vite-project/src/components/pages/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Login() {
+  const [ data, setData ] = useState({
+    email: '',
+    password: ''
+  })
+
+  const loginUser = (e) => {
+    e.preventDefault() // prevent page refresh
+  }
   return (
     <div>
-      Login
+      <form onSubmit={loginUser}>
+        <input
+          type="email"
+          placeholder='enter email ...'
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label>Password: </label>
+        <input
+          type="password"
+          placeholder='enter password ...'
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
