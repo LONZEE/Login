2fbc80b0fc6c3fe4216cbc91ff73901ed71e11e8
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-16 14:52:27 -0500

    added mongoose

--- client/server/index.js
+++ client/server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const {mongoose} = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
 
 const app = express();
 app.use('/', require('./routes/auth'));
